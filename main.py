cecc29efcfcad7a214d898cbc6e2dd626218d636
luizfrborges <luizfrgorges@yahoo.com.br>
2020-11-23 16:23:14 -0300

    23-11-20

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from lib.mineiro import Mineiro
 from lib.analista import Analista
 
@@ -16,5 +12,3 @@ if __name__ == '__main__':
     analista.analise_csv()
     print(mineiro.contador)
 
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
